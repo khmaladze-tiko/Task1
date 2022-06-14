5e153d36c033b52598a1a0ee930eed23ef6310fe
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2022-06-14 21:42:30 +0400

    add first task

---
+++ task1.js
@@ -0,0 +1,13 @@
+// ჩაანაცვლეთ if-else ტერნარული ოპერატორით
+// let result;
+// if (x + y < 4) {
+//   result = true;
+// } else {
+//   result = false;
+// }
+
+
+
+// პასუხი
+
+let result = x + y < 4 ? true : false;
