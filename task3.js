0c5ea9551b597936b593d5e1f7ab3bea72a877a5
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2022-06-15 00:07:46 +0400

    add task3

---
+++ task3.js
@@ -0,0 +1,34 @@
+// Task 3 
+
+// ჩაანაცვლეთ If-Else switch statement-ით
+
+// switch (val) {
+//     case 1:
+//       console.log( 1 );
+//       break;
+  
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//       console.log( '+' );
+//       break;
+  
+//     default:
+//       console.log( 0 );
+//   }
+
+let i;
+if ( i = 1 ) {
+    console.log( 1 )
+} else if ( i = 2 ) {
+
+} else if ( i = 3 ) {
+
+} else if ( i = 4 ) {
+
+} else if ( i = 5 ) {
+    console.log( '+' )
+} else {
+    console.log( 0 )
+};
