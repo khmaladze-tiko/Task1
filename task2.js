f0a28b2bc098977fd55d6f1b555c3eea0b711812
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2022-06-14 23:22:42 +0400

    add task2

---
+++ task2.js
@@ -0,0 +1,24 @@
+// Task2
+// ჩაანაცვლეთ if-else ტერნარული ოპერატორით
+
+
+// let color;
+// let message;
+
+// if (color == 'Red') {
+//   message = 'R';
+// } else if (color == 'Yellow') {
+//   message = 'Y';
+// } else if (color == '') {
+//   message = 'none';
+// } else {
+//   message = undefined;
+// }
+
+// პასუხი
+
+let color;
+let message = color == 'Red' ? message = 'R' 
+    : color = 'Yellow' ? message = 'Y' 
+    : color == '' ? message = 'none' 
+    : message = undefined;
