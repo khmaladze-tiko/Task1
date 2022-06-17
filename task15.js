bdf65b1978df53a5ab8147d784ae40a25e0f7540
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2022-06-18 02:38:59 +0400

    task15

---
+++ task15.js
@@ -0,0 +1,29 @@
+//დაწერეთ ფუნქცია weekDay , რომელიც გადაცემული ციფრის 
+//მიხედვით დააბრუნებს კვირის დღეს,
+//აუცილებელია შემოწმება რომ არ გადაეცეს 0 ან 7-ზე მეტი ციფრი, 
+//ასევე პარამეტრი აუცინებლად უნდა იყოს 
+// number, წინააღმდეგ შემთხვევაში დალოგოს შესამაბისი ერორ მესიჯი
+
+//გამოძახების მაგალითი
+
+// weekDay(1) // Monday
+// weekDay('1') // Error, not a number
+// weekDay(8) // Error, number should be in range of 1 to 7
+
+
+// const dayNames = [
+//     'sun',
+//     'mon',
+//     'tue',
+//     'wed',
+//     'thu',
+//     'fri',
+//     'set'
+// ];
+// let i = 0;
+// while (i < 7 && startDate < endDate) {
+//     dayNames[startDate.day()] = dayNames.length;
+// }
+// console.log()
+
+ 
